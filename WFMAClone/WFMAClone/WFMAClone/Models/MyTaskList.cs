e441a6b4aac7cdf2b45dc974eab793fbb63f895d
Janko Žabojec <jz49497@fer.hr>
2018-07-10 15:33:07 +0200

    color and short date fixed

--- WFMAClone/WFMAClone/WFMAClone/Models/MyTaskList.cs
+++ WFMAClone/WFMAClone/WFMAClone/Models/MyTaskList.cs
@@ -14,15 +14,42 @@ namespace WFMAClone
 		public int TaskPriority { get; set; }
 		public string Type { get; set; }
 		public DateTime DueDate { get; set; }
+		public string Color
+		{
+			get
+			{
+				if (Status.Equals("new"))
+				{
+					return "Red";
+				}
+				else if (Status.Equals("accepted"))
+				{
+					return "Orange";
+				}
+				else if (Status.Equals("downloaded"))
+				{
+					return "Lime";
+				}
+				else if (Status.Equals("not downloaded"))
+				{
+					return "Blue";
+				}
+				else
+				{
+					return "Black";
+				}
+			}
+		}
+		public string ShortDate
+		{
+			get
+			{
+				var temp = DueDate.Date;
 
-        public string Color
-        {
-            get
-            {
-                return "red";
-            }
-        }
-    }
+				return temp.ToString("dd.M.yyyy");
+			}
+		}
+	}
 
 	class TaskList
 	{
