a47a1a8e73f470311e3ad4371c7d751f544cdccf
antonio-mikulic <antonio.mikulic@outlook.com>
2018-07-11 13:29:38 +0200

    * MainPage.xaml: Added filter by date
    * MainPage.xaml.cs: Added datePicker

--- WFMAClone/WFMAClone/WFMAClone/MainPage.xaml.cs
+++ WFMAClone/WFMAClone/WFMAClone/MainPage.xaml.cs
@@ -4,50 +4,81 @@ using Xamarin.Forms;
 
 namespace WFMAClone
 {
-	public partial class MainPage : ContentPage
-	{
+    public partial class MainPage : ContentPage
+    {
+        private static List<MyTaskList> notFilteredTaskList = new List<MyTaskList>();
+
+
         public MainPage()
-		{
-			InitializeComponent();
-		}
+        {
+            InitializeComponent();
+        }
 
-		protected override async void OnAppearing()
-		{
-			base.OnAppearing();
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
 
-			RestService restService = new RestService();
+            WFMAClone.RestService restService = new WFMAClone.RestService();
 
-			TaskList taskList = await restService.RefreshDataAsync();
-			listView.ItemsSource = taskList.Tasks;
-		}
+            TaskList taskList = await restService.RefreshDataAsync();
+
+            if (taskList.Tasks != null){
+                notFilteredTaskList = taskList.Tasks;
+                listView.ItemsSource = taskList.Tasks;
+            }
+            else{
+                Console.WriteLine("taskList.Tasks is empty! Can't set listView.ItemsSource");
+                Console.ReadLine();
+            }
+        }
 
 
-		async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
-		{
-			//((App)App.Current).ResumeAtTodoId = (e.SelectedItem as TodoItem).ID;x
-			//Debug.WriteLine("setting ResumeAtTodoId = " + (e.SelectedItem as TodoItem).ID);
-			if (e.SelectedItem != null)
-			{
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            //((App)App.Current).ResumeAtTodoId = (e.SelectedItem as TodoItem).ID;x
+            //Debug.WriteLine("setting ResumeAtTodoId = " + (e.SelectedItem as TodoItem).ID);
+            if (e.SelectedItem != null)
+            {
                 var task = e.SelectedItem as MyTaskList;
                 await Navigation.PushModalAsync(new MyTaskPage(task.Id));
 
-                /*
-                await Navigation.PushModalAsync(new MyTaskPage
-                {
-                    BindingContext = e.SelectedItem as MyTask
-                });
-                */
+
+                //await Navigation.PushModalAsync(new MyTaskPage{
+                //    BindingContext = e.SelectedItem as MyTask
+                //});
+
             }
-		}
-        void OnDateFilterButtonClicked(object sender, EventArgs e)
-        {
-        }
-        void OnQuickFilterButtonClicked(object sender, EventArgs e)
-        {
-        }
-        void OnRemoveFilterButtonClicked(object sender, EventArgs e)
-        {
         }
 
-	}
+        void OnDateFilterButtonClicked(object sender, EventArgs e)
+        {
+            picker.Focus();
+        }
+
+        void DatePicker_OnDateSelected(object sender, DateChangedEventArgs e)
+        {
+            List<MyTaskList> filteredList = new List<MyTaskList>();
+            foreach (var Task in notFilteredTaskList)
+            {
+                if (Task.DueDate.Date.Equals(e.NewDate)){
+                    Console.WriteLine("Task date is same!");
+                    Console.ReadLine();
+                    filteredList.Add(Task);
+                }
+            }
+            listView.ItemsSource = filteredList;
+        }
+
+        void OnQuickFilterButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        void OnRemoveFilterButtonClicked(object sender, EventArgs e)
+        {
+            listView.ItemsSource = notFilteredTaskList;
+
+        }
+
+    }
 }
