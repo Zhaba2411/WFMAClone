ae98dadff35e6f7002ba1e7365d5f3f0afcffe32
Bole <jelenabole@gmail.com>
2018-07-11 21:29:27 +0200

    Menu items to img, added gestures

--- WFMAClone/WFMAClone/WFMAClone/DocumentDetailsPage.xaml.cs
+++ WFMAClone/WFMAClone/WFMAClone/DocumentDetailsPage.xaml.cs
@@ -21,8 +21,26 @@ namespace WFMAClone
                 OnNavigateButtonClicked(s, e);
             };
             backButton.GestureRecognizers.Add(tapGestureRecognizer);
+
+            // bottom menu - delete button:
+            tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += (s, e) => {
+                // accept (?) action
+                Console.WriteLine("accept doc");
+            };
+            AcceptButton.GestureRecognizers.Add(tapGestureRecognizer);
+
+            // bottom menu - delete button:
+            tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += (s, e) => {
+                // delete this document
+                Console.WriteLine("delete doc");
+            };
+            DeleteButton.GestureRecognizers.Add(tapGestureRecognizer);
+
         }
 
         async void OnNavigateButtonClicked(object sender, EventArgs e) {             await Navigation.PopModalAsync();         }
+
     }
 }
