493f420bbd8f56a16e31580040d0a8c97a8e95d1
Bole <jelenabole@gmail.com>
2018-07-12 14:26:33 +0200

    Frontend changes (main page rewritten)

--- WFMAClone/WFMAClone/WFMAClone/MainPage.xaml.cs
+++ WFMAClone/WFMAClone/WFMAClone/MainPage.xaml.cs
@@ -8,82 +8,56 @@ namespace WFMAClone
     {
         private static List<MyTask> notFilteredTaskList = new List<MyTask>();
 
-
-        public MainPage()
-        {
+        public MainPage() {
             InitializeComponent();
         }
 
-        protected override async void OnAppearing()
-        {
+        protected override async void OnAppearing() {
             base.OnAppearing();
 
-            WFMAClone.RestService restService = new WFMAClone.RestService();
-
+            RestService restService = new RestService();
             MyTaskList taskList = await restService.getTaskListAsync();
 
-            if (taskList.Tasks != null){
+            if (taskList.Tasks != null) {
                 notFilteredTaskList = taskList.Tasks;
                 listView.ItemsSource = taskList.Tasks;
             }
-            else{
+            else {
                 Console.WriteLine("taskList.Tasks is empty! Can't set listView.ItemsSource");
-                Console.ReadLine();
             }
         }
 
-
-        async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            //((App)App.Current).ResumeAtTodoId = (e.SelectedItem as TodoItem).ID;x
-            //Debug.WriteLine("setting ResumeAtTodoId = " + (e.SelectedItem as TodoItem).ID);
-            if (e.SelectedItem != null)
-            {
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs e) {
+            if (e.SelectedItem != null) {
                 var task = e.SelectedItem as MyTask;
                 await Navigation.PushModalAsync(new MyTaskPage(task.Id));
-
-
-                //await Navigation.PushModalAsync(new MyTaskPage{
-                //    BindingContext = e.SelectedItem as MyTask
-                //});
-
             }
         }
 
-        void OnDateFilterButtonClicked(object sender, EventArgs e)
-        {
+        void OnDateFilterButtonClicked(object sender, EventArgs e) {
             picker.Focus();
         }
 
-        void DatePicker_OnDateSelected(object sender, DateChangedEventArgs e)
-        {
+        void DatePicker_OnDateSelected(object sender, DateChangedEventArgs e) {
             List<MyTask> filteredList = new List<MyTask>();
-            foreach (var Task in notFilteredTaskList)
-            {
-                if (Task.DueDate.Date.Equals(e.NewDate)){
+            foreach (var Task in notFilteredTaskList) {
+                if (Task.DueDate.Date.Equals(e.NewDate)) {
                     Console.WriteLine("Task date is same!");
-                    Console.ReadLine();
                     filteredList.Add(Task);
                 }
             }
             listView.ItemsSource = filteredList;
         }
 
-		async void OnQuickFilterButtonClicked(object sender, EventArgs e)
-		{
+		async void OnQuickFilterButtonClicked(object sender, EventArgs e) {
 			var action = await DisplayActionSheet("Quick Filters", "Cancel", "", "New", "Accepted", "Downloaded", "Not Downloaded");
 			action = action.ToLower();
+
 			Console.WriteLine(action);
-			if (String.IsNullOrWhiteSpace(action) || action.Equals("Cancel"))
-			{
-			}
-			else
-			{
+			if (!(String.IsNullOrWhiteSpace(action) || action.Equals("Cancel"))) {
 				List<MyTask> filteredList = new List<MyTask>();
-				foreach (var task in notFilteredTaskList)
-				{
-					if (task.Status.Equals(action))
-					{
+				foreach (var task in notFilteredTaskList) {
+					if (task.Status.Equals(action)) {
 						filteredList.Add(task);
 					}
 				}
@@ -91,10 +65,8 @@ namespace WFMAClone
 			}
 		}
 
-		void OnRemoveFilterButtonClicked(object sender, EventArgs e)
-        {
+		void OnRemoveFilterButtonClicked(object sender, EventArgs e) {
             listView.ItemsSource = notFilteredTaskList;
         }
-
     }
 }
